d51514337b58c5f0453d3264b6befb983dd26585
esakk <esakk@192.168.0.20>
2019-03-12 21:03:30 -0400

    Zuul

---
+++ Mhss-ZuulProxy/src/main/java/com/javatechdeveloper/mhss/MhssZuulProxyApplication.java
@@ -0,0 +1,22 @@
+package com.javatechdeveloper.mhss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@EnableEurekaClient
+@EnableZuulProxy
+@SpringBootApplication
+public class MhssZuulProxyApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(MhssZuulProxyApplication.class, args);
+	}
+
+}
---
+++ Mhss-ZuulProxy/src/test/java/com/javatechdeveloper/mhss/MhssZuulProxyApplicationTests.java
@@ -0,0 +1,16 @@
+package com.javatechdeveloper.mhss;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MhssZuulProxyApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
